f92385607a5e11c0447852efeb352691a2827fbf
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-12 03:18:02 +0200

    Create btc-connect-init.js

---
+++ assets/js/btc-connect-init.js
@@ -0,0 +1,9 @@
+function btcConnectLogin() {
+    // This function would trigger the Bitcoin wallet connection process
+    console.log("Trigger wallet connection");
+}
+
+function btcZap() {
+    // This function would handle the Zap functionality to send sats
+    console.log("Send sats to the post");
+}
